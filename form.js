0467b07e8465f39a6f2001889b3bd0bbc5adbdf0
mulimuoki001 <b.muoki@alustudent.com>
2023-12-10 11:20:12 +0200

    Connected contact form with email

---
+++ form.js
@@ -0,0 +1,187 @@
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+
+
+function sendEmail() {
+    const bodyMessage = `Name: ${fullName.value}<br> Email: ${email.value}<br> Phone: ${phone.value}<br> Subject: ${subject.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "goodsamaritan845@gmail.com",
+        Password: "DBB520D10EE78E979E95CB528EAD02CFA82F",
+        To: 'gsamaritan837@gmail.com',
+        From: "goodsamaritan845@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Good job!",
+                    text: "You clicked the button!",
+                    icon: "success"
+                });
+            }
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value === "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+        item.addEventListener("keyup", () => {
+            if (item.value !== "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!emailRegex.value.match(emailRegex)) {
+        errorTxtEmail.style.display = "block";
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value !== "") {
+            errorTxtEmail.innerText = "Please provide a valid email";
+        }
+        else {
+            errorTxtEmail.innerText = "Email cannot be blank";
+        }
+
+    }
+    else {
+        errorTxtEmail.style.display = "none";
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- form.php
+++ form.php
@@ -4,17 +4,5 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@mulimuoki.tech';
-$email_subject = 'New Form Submission';
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"Subject: $subject.\n".
-"User Message: $message.\n";
 
-$to = 'goodsamaritan845@gmail.com';
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $email \r\n";
-
-mail($to, $subject, $email_body, $headers);
 ?>
